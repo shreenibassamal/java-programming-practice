68729154f846fdf666d0185dfc79e682dbdbe8d5
shreenibassamal <shreenibassamal@gmail.com>
2025-05-18 04:07:26 +0530

    frame work added

---
+++ java_coding/src/com/programming/number/AddTwoNumber.java
@@ -0,0 +1,12 @@
+package com.programming.number;
+
+class AddTwoNumber {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        int c= a+b;
+        System.out.println(c);
+
+    }
+
+}
